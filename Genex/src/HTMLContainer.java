f59fdd6a6d988e9c15c77b5bf49ea85d9945410b
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2004-12-14 14:00:35 +0000

    part 1 of refactoring - gene.java
    git-svn-id: file:///usr/local/SVN/trunk@12 f794847c-437f-4cdd-b6dc-fa27500bcbe7

---
+++ Genex/src/HTMLContainer.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Oct 12, 2004
+ *
+ */
+
+/**
+ * @author Brian.White
+ *
+ */
+public class HTMLContainer {
+	final String _colorHTMLString;
+	final String _bwHTMLString;
+	
+	HTMLContainer (String color, String bw) {
+		_colorHTMLString = color;
+		_bwHTMLString = bw;
+	}
+	
+	public String getColorHTML() {
+		return _colorHTMLString;
+	}
+	
+	public String getBwHTML() {
+		return _bwHTMLString;
+	}
+
+}
