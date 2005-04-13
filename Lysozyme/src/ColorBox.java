f1c565484da85b13d5b3f85e8d823dab8ee40ce9
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2005-04-13 13:08:47 +0000

    added buildfile and full rows & cols
    git-svn-id: file:///usr/local/SVN/trunk@14 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- Lysozyme/src/ColorBox.java
+++ Lysozyme/src/ColorBox.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,33 +15,40 @@ import javax.swing.JPanel;
  * @author Brian.White
  */
 public class ColorBox extends JPanel {
-	final Color[] ColorList 
-	              = {Color.white, Color.red, Color.green, Color.blue};
-	final String[] ChoiceList 
-	              = {"-", "R", "G", "B"};
+	public final Color[] colors;
+	public final String[] choices;
 	
 	JComboBox comboBox;
 	
-	public ColorBox(){ 
-		comboBox= new JComboBox(ChoiceList);
+	public ColorBox(Color[] colorList, String[] choiceList){ 
+
+		colors = colorList;
+		choices = choiceList;
+		
+		comboBox = new JComboBox(choices);
 		
 		comboBox.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				JComboBox cb = (JComboBox)e.getSource();
 				String choice = (String)cb.getSelectedItem();
 				Color chosenColor = Color.white;
-				for (int i = 0; i < ChoiceList.length; i++) {
-					if (choice.equals(ChoiceList[i])) {
-						chosenColor = ColorList[i];
+				for (int i = 0; i < choices.length; i++) {
+					if (choice.equals(choices[i])) {
+						chosenColor = colors[i];
 						break;
 					}
 				}
+				comboBox.setBackground(chosenColor);
 				comboBox.getParent().setBackground(chosenColor);
 			}
 		});
 		
 		
 		this.add(comboBox);
+		comboBox.setBackground(Color.white);
+		comboBox.getParent().setBackground(Color.white);
+		comboBox.getParent().setSize(new Dimension(100,100));
+
 	}
 	
 	
