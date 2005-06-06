fb419e462251dfcb6e1956354d12b45ff289a0cf
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2005-06-06 18:05:57 +0000

    add index.php and savetoserver icons
    git-svn-id: file:///usr/local/SVN/trunk@36 f794847c-437f-4cdd-b6dc-fa27500bcbe7

---
+++ VGL/index.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+<title>Saved VGL Problems from 
+<?php
+  $dir = getcwd();
+  echo ereg_replace('/', '', strrchr($dir,"/"));
+?>
+</title>
+<script language="JavaScript">
+var newWindow;
+function showVGL(fileName, shortName) {
+	if (newWindow != null) {
+		newWindow.close();
+    }
+	newWindow = window.open("","", "height=200, width=400");
+	newWindow.document.writeln("<html><head></head>");
+	newWindow.document.writeln("This is VGL Problem: " + shortName + "<br><br>");
+	newWindow.document.writeln("<applet code=\"VGLApplet.class\"");
+	newWindow.document.writeln("archive=\"../VGL.jar\" name=\"VGL\" ");
+	newWindow.document.writeln("width=350 height=150>");
+	newWindow.document.writeln("<PARAM NAME=\"workFile\" VALUE=\"http://intro.bio.umb.edu/" + fileName + "\">");
+    newWindow.document.writeln("</applet> <br>");
+	newWindow.document.writeln("</body></html>");
+}
+
+</script>
+</head>
+<body bgcolor=lightblue onunload="newWindow.close()">
+<center>
+<font size=+3>
+Saved VGL Problems from 
+<?php
+  $dir = getcwd();
+  echo ereg_replace('/', '', strrchr($dir,"/"));
+?>
+</font>
+<br><br>
+</center>
+
+<form>
+<font size=+2 color=red>Click on one of the buttons below to run your VGL problem:</font><br><br>
+<font color=blue><b><u>Notes</b></u>:
+<ul>
+  <li>Only load one problem at a time.</li>
+  <li>If you have loaded more than one problem and the buttons stop working, try re-loading this page.</li>
+  <li>You should allow printing when asked or the program will not run.</li>
+  <li>The Cages will all appear on top of each other; you will have to drag them off of one another.</li>
+  <li>You may have to quit the browser to get all the VGL windows and cages to disappear.</li>
+</ul>
+</font>
+<br>
+<b><u>Saved Problems</b></u>:
+<ul>
+<?php 
+  $dh = opendir('.');
+  while (false != ($fileName = readdir($dh))) {
+    if (false != ereg('.wrk', $fileName)) {
+      $files[] = $fileName;
+    }
+  }
+  closedir($dh);
+  
+  sort($files);
+  
+  $dirName = ereg_replace('Documents/', '', strstr(getcwd(), 'Documents/'));
+  
+  foreach ($files As $fileName) {
+    $shortName = ereg_replace('.prb', '', $fileName);
+    echo "<li><input type=\"button\" value=\"$shortName\" onClick=\"showVGL('{$dirName}/{$fileName}', '$shortName')\"></li>\n";
+  }
+?>
+</ul>
+
+</form>
+
+</body>
+</html>
