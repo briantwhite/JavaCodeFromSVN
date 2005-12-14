e23aec2636152f7338bb9920b9b8156effc1fa57
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2005-12-14 03:00:45 +0000

    second initial commit
    git-svn-id: file:///usr/local/SVN/trunk@73 f794847c-437f-4cdd-b6dc-fa27500bcbe7

---
+++ WordTriples/src/ArrayTest.java
@@ -0,0 +1,5 @@
+import cern.colt.matrix.impl.SparseDoubleMatrix3D;
+
+public class ArrayTest {
+    SparseDoubleMatrix3D testMatrix = new SparseDoubleMatrix3D(100,100,100);
+}
---
+++ WordTriples/src/WordTriples.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class WordTriples extends JFrame {
+
+	public WordTriples () {
+		super("Word Triples Analyzer");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		WordTriples wordTriples = new WordTriples();
+		wordTriples.show();
+
+	}
+
+}
