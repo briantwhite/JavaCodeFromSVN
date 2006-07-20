cfe5f068f992803f3f97912156fb78d914fbda23
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2006-07-20 18:50:54 +0000

    got word doubles to work with sparse matrix
    git-svn-id: file:///usr/local/SVN/trunk@110 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- WordTriples/src/SaveWordPairsAsArffUI.java
+++ WordTriples/src/SaveWordPairsAsArffUI.java
@@ -27,8 +27,8 @@ public class SaveWordPairsAsArffUI extends JPanel {
 	private JScrollPane scrollPane;
 	private JButton saveFileButton;
 	private JFileChooser saveFileChooser;
-
-
+	
+	
 	public SaveWordPairsAsArffUI() {
 		super();
 		infoLabel = new InfoLabel("Hi there");
@@ -49,7 +49,7 @@ public class SaveWordPairsAsArffUI extends JPanel {
 		scoresTable = new JTable(sorter);
 		scoresTable.setShowGrid(true);
 		scoresTable.setGridColor(Color.BLACK);
-
+		
 		sorter.setTableHeader(scoresTable.getTableHeader());
 		scrollPane = new JScrollPane(scoresTable);
 		this.add(scrollPane, BorderLayout.CENTER);	
@@ -73,13 +73,13 @@ public class SaveWordPairsAsArffUI extends JPanel {
 	}
 	
 	public void setHypsAndScores(ArrayList hypotheses, 
-								 HashMap wordCodeMap,
-								 TreeMap pairMap){
-			final ArrayList hyps = hypotheses;
-			final HashMap wcMap = wordCodeMap;
-			final TreeMap pMap = pairMap;
-			
-			
+			HashMap wordCodeMap,
+			TreeMap pairMap){
+		final ArrayList hyps = hypotheses;
+		final HashMap wcMap = wordCodeMap;
+		final TreeMap pMap = pairMap;
+		
+		
 		saveFileButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				saveArffFile(hyps, wcMap, pMap);
@@ -89,94 +89,127 @@ public class SaveWordPairsAsArffUI extends JPanel {
 	}
 	
 	public void saveArffFile(ArrayList hypotheses, 
-							HashMap wordCodeMap,
-							TreeMap pairMap) {
-		// make the header
-		StringBuffer headerBuffer = new StringBuffer();
-		headerBuffer.append("@RELATION score\n\n");
+			HashMap wordCodeMap,
+			TreeMap pairMap) {
 		
-		Iterator scoreIt = pairMap.keySet().iterator();
-		while (scoreIt.hasNext()){
-			headerBuffer.append("@ATTRIBUTE " 
-					+ (String)scoreIt.next()
-					+ " {0,1} \n");
-		}
-		
-		headerBuffer.append("@ATTRIBUTE class {");
-		StringBuffer possibleScores = new StringBuffer();
-		for (int row = 0; row < scoreShiftTableModel.getRowCount(); row++) {
-			possibleScores.append(scoreShiftTableModel.getValueAt(row,2)
-					+ ",");
-		}
-		headerBuffer.append(
-				(possibleScores.deleteCharAt(possibleScores.length() - 1).toString()));
-		headerBuffer.append("}\n");
-		
-		headerBuffer.append("@DATA \n");
-
-		// make score conversion hash table
-		TreeMap newScoreMap = new TreeMap();
-		for (int row = 0; row < scoreShiftTableModel.getRowCount(); row++) {
-			newScoreMap.put((Integer)scoreShiftTableModel.getValueAt(row,0),
-					(Integer)scoreShiftTableModel.getValueAt(row,2));
-		}
-
-		Iterator hypIterator = hypotheses.iterator();
-		TreeMap scoreMap = new TreeMap();
-		TreeMap scoreCounts = new TreeMap();
-		while (hypIterator.hasNext()){
-			Hypothesis hypothesis = (Hypothesis)hypIterator.next();
-			Integer score = new Integer(hypothesis.getScore());
-			scoreMap.put(score, score);
-			if (!scoreCounts.containsKey(score)){
-				scoreCounts.put(score, new Integer(1));
-			} else {
-				int oldCount = ((Integer)scoreCounts.get(score)).intValue();
-				scoreCounts.put(score, new Integer(oldCount + 1));
+		//set up array of words listed by their code
+		//first, find the max code num
+		Iterator wcIt = wordCodeMap.keySet().iterator();
+		int maxCode = 0;
+		while (wcIt.hasNext()) {
+			if (((Integer)wordCodeMap.get(wcIt.next())).intValue() > maxCode) {
+				maxCode = ((Integer)wordCodeMap.get(wcIt.next())).intValue();
 			}
 		}
+		wcIt = wordCodeMap.keySet().iterator();
+		String[] wordList = new String[maxCode + 10];
+		while (wcIt.hasNext()) {
+			String word = (String)wcIt.next();
+			wordList[((Integer)wordCodeMap.get(word)).intValue()] = word;
+		}
 		
-		createTable(scoreMap.size());
-		Iterator scoreIterator = scoreMap.keySet().iterator();
-		int rowNumber = 0;
-		while (scoreIterator.hasNext()){
-			Integer currentScore = (Integer)scoreIterator.next();
-			addScore(rowNumber, 
-					currentScore.intValue(),
-					((Integer)scoreCounts.get(currentScore)).intValue());
-			rowNumber++;
-		}
-
-		StringBuffer scoresBuffer = new StringBuffer();
-		Iterator hypIt = hypotheses.iterator();
-		while (hypIt.hasNext()){
-			TreeMap workingMap = new TreeMap(pairMap);
-			Hypothesis hyp = (Hypothesis)hypIt.next();
-			int[] wordCodeSet = hyp.getCodeList(wordCodeMap);
-			for (int i = 0; i < (wordCodeSet.length - 1); i++) {
-				String key = wordCodeSet[i] + "-" + wordCodeSet[i+1];
-				if (workingMap.containsKey(key)) {
-					workingMap.put(key, new Integer(1));
-				}
-			}
-			
-			scoreIterator = workingMap.keySet().iterator();
-			while (scoreIterator.hasNext()) {
-				String key = (String)scoreIterator.next();
-				scoresBuffer.append((Integer)workingMap.get(key) + ",");
-			}
-			Integer originalScore = new Integer(hyp.getScore());
-			Integer newScore = (Integer)newScoreMap.get(originalScore);
-			scoresBuffer.append(newScore);
-			scoresBuffer.append("\n");
-		}
+		HashMap pairAttributeNumberMap = new HashMap();
 		
 		FileWriter arffFileWriter = null;
 		if (saveFileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+						
+			//save the file
 			try {
 				arffFileWriter = new FileWriter(saveFileChooser.getSelectedFile());
-				arffFileWriter.write(headerBuffer.toString());
-				arffFileWriter.write(scoresBuffer.toString());	
+				
+				// make the header and create a mapping of pair codes  11-123 to attribute #s
+				arffFileWriter.write("@RELATION score\n\n");
+				
+				Iterator scoreIt = pairMap.keySet().iterator();
+				int attributeNum = 0;
+				while (scoreIt.hasNext()){
+					String attributeCode = (String)scoreIt.next();
+					String[] attributeCodeParts = attributeCode.split("-");
+					String firstWord = wordList[Integer.parseInt(attributeCodeParts[0])];
+					String secondWord = wordList[Integer.parseInt(attributeCodeParts[1])];
+					arffFileWriter.write("@ATTRIBUTE " 
+							+ firstWord + "-" + secondWord
+							+ " {0,1} \n");
+					arffFileWriter.write("%    attrubute #: " + attributeNum
+							+ " code: " + attributeCode + "\n");
+					pairAttributeNumberMap.put(attributeCode, new Integer(attributeNum));
+					attributeNum++;
+				}
+				
+				arffFileWriter.write("@ATTRIBUTE class {");
+				StringBuffer possibleScores = new StringBuffer();
+				for (int row = 0; row < scoreShiftTableModel.getRowCount(); row++) {
+					possibleScores.append(scoreShiftTableModel.getValueAt(row,2)
+							+ ",");
+				}
+				arffFileWriter.write(
+						(possibleScores.deleteCharAt(possibleScores.length() - 1).toString()));
+				arffFileWriter.write("}\n");
+				
+				arffFileWriter.write("@DATA \n");
+				
+				// make score conversion hash table
+				TreeMap newScoreMap = new TreeMap();
+				for (int row = 0; row < scoreShiftTableModel.getRowCount(); row++) {
+					newScoreMap.put((Integer)scoreShiftTableModel.getValueAt(row,0),
+							(Integer)scoreShiftTableModel.getValueAt(row,2));
+				}
+				
+				Iterator hypIterator = hypotheses.iterator();
+				TreeMap scoreMap = new TreeMap();
+				TreeMap scoreCounts = new TreeMap();
+				while (hypIterator.hasNext()){
+					Hypothesis hypothesis = (Hypothesis)hypIterator.next();
+					Integer score = new Integer(hypothesis.getScore());
+					scoreMap.put(score, score);
+					if (!scoreCounts.containsKey(score)){
+						scoreCounts.put(score, new Integer(1));
+					} else {
+						int oldCount = ((Integer)scoreCounts.get(score)).intValue();
+						scoreCounts.put(score, new Integer(oldCount + 1));
+					}
+				}
+				
+				createTable(scoreMap.size());
+				Iterator scoreIterator = scoreMap.keySet().iterator();
+				int rowNumber = 0;
+				while (scoreIterator.hasNext()){
+					Integer currentScore = (Integer)scoreIterator.next();
+					addScore(rowNumber, 
+							currentScore.intValue(),
+							((Integer)scoreCounts.get(currentScore)).intValue());
+					rowNumber++;
+				}
+				
+				Iterator hypIt = hypotheses.iterator();
+				while (hypIt.hasNext()){
+					
+					arffFileWriter.write("{");
+					
+					TreeMap workingMap = new TreeMap(pairMap);
+					Hypothesis hyp = (Hypothesis)hypIt.next();
+					int[] wordCodeSet = hyp.getCodeList(wordCodeMap);
+					for (int i = 0; i < (wordCodeSet.length - 1); i++) {
+						String key = wordCodeSet[i] + "-" + wordCodeSet[i+1];
+						if (workingMap.containsKey(key)) {
+							workingMap.put(key, new Integer(1));
+						}
+					}
+					
+					scoreIterator = workingMap.keySet().iterator();
+					while (scoreIterator.hasNext()) {
+						String key = (String)scoreIterator.next();
+						int score = ((Integer)workingMap.get(key)).intValue();
+						int attrNum = ((Integer)pairAttributeNumberMap.get(key)).intValue();
+						if (score == 1) {
+							arffFileWriter.write(attrNum + " 1, ");
+						}
+					}
+					Integer originalScore = new Integer(hyp.getScore());
+					Integer newScore = (Integer)newScoreMap.get(originalScore);
+					arffFileWriter.write(attributeNum + " " + newScore.toString() + "}\n");
+				}
+				
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
