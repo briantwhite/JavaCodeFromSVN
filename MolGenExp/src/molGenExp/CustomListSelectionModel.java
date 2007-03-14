d6c55a5bcd53f09321ed7677d20601f8614bccef
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2007-03-14 18:12:57 +0000

    saved DNA sequences of key organisms in development logcommented out print statements in CustomList...saved as version 0.8
    git-svn-id: file:///usr/local/SVN/trunk@177 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- MolGenExp/src/molGenExp/CustomListSelectionModel.java
+++ MolGenExp/src/molGenExp/CustomListSelectionModel.java
@@ -10,17 +10,17 @@ public class CustomListSelectionModel extends DefaultListSelectionModel {
 	}
 
 	public void setSelectionInterval(int index0, int index1) {
-		System.out.println("set interval: " + location + " " + index0 + " " + index1);
+//		System.out.println("set interval: " + location + " " + index0 + " " + index1);
 		super.setSelectionInterval(index0, index1);
 	}
 	
 	public void addSelectionInterval(int index0, int index1) {
-		System.out.println("add interval: " + location + " " + index0 + " " + index1);
+//		System.out.println("add interval: " + location + " " + index0 + " " + index1);
 		super.addSelectionInterval(index0, index1);		
 	}
 	
 	public void removeSelectionInterval(int index0, int index1) {
-		System.out.println("remove interval: " + location + " " + index0 + " " + index1);
+//		System.out.println("remove interval: " + location + " " + index0 + " " + index1);
 		super.removeSelectionInterval(index0, index1);
 	}
 }
