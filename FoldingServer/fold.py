2a577ef9f4db17bd4d0f99b69a74971b7b9be868
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2008-02-14 15:46:30 +0000

    new fold.py
    git-svn-id: file:///usr/local/SVN/trunk@319 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- FoldingServer/fold.py
+++ FoldingServer/fold.py
@@ -10,8 +10,8 @@ result = []
 for sequence in local_sequences:
 	cmd = "java -Djava.awt.headless=true -cp FoldingServer.jar foldingServer.FoldingServer -c " + sequence
 	for line in os.popen(cmd).readlines():
-		if line.find("Color") >= 0: 
-			message = str(mpi.rank) + ":" + sequence +  ";" + line.rstrip("\n")
+		if line.find(":") >= 0: 
+			message = str(mpi.rank) + ";" + sequence +  ";" + line.rstrip("\n")
 			result.append(message)
 
 all = mpi.gather(result)
