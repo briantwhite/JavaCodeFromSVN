7f111c070dbd6c0ff728cfd55b5d8c74002a5097
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2008-05-08 19:12:47 +0000

    more
    git-svn-id: file:///usr/local/SVN/trunk@351 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- VGLII/src/GeneticModels/Character.java
+++ VGLII/src/GeneticModels/Character.java
@@ -1,8 +1,9 @@
 package GeneticModels;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public abstract class Character {
+public class Character {
 
 	ArrayList<TraitSet> possibleTraitSets;
 	
@@ -10,7 +11,20 @@ public abstract class Character {
 		possibleTraitSets = new ArrayList();
 	}
 	
+	public void add(TraitSet ts) {
+		possibleTraitSets.add(ts);
+	}
+	
 	public int geteNumberOfMembers() {
 		return possibleTraitSets.size();
+	}
+	
+	//get TraitSet and remove it from list to prevent re-use
+	public TraitSet getRandomTraitSet() {
+		Random r = new Random();
+		int i = r.nextInt(possibleTraitSets.size());
+		TraitSet ts = possibleTraitSets.get(i);
+		possibleTraitSets.remove(i);
+		return ts;
 	}
 }
