d7d760e1088e7f2ee0639c0171947b0036f97d10
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2008-07-30 19:46:48 +0000

    working on it
    git-svn-id: file:///usr/local/SVN/trunk@476 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- old_PS/src/phylogenySurvey/SurveyUI.java
+++ old_PS/src/phylogenySurvey/SurveyUI.java
@@ -1,7 +1,11 @@
 package phylogenySurvey;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class SurveyUI {
@@ -9,12 +13,22 @@ public class SurveyUI {
 	private Container masterContainer;
 	private JPanel workPanel;
 	
+	private JLabel testLabel;
+	
 	public SurveyUI(Container masterContainer) {
 		this.masterContainer = masterContainer;
 	}
 	
 	public void setupUI() {
 		workPanel = new JPanel();
+		workPanel.setPreferredSize(new Dimension(1000,1000));
+		workPanel.setLayout(null);
+		
+		testLabel = new JLabel("HI FRED");
+		testLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		workPanel.add(testLabel);
+		testLabel.setLocation(100, 100);
+		
 		masterContainer.add(workPanel);
 	}
 
