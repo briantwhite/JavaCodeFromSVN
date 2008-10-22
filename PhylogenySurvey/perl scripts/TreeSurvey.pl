6e9e84bad0746c1c37ce8294c745095f4afc2d44
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2008-10-22 18:40:24 +0000

    working on perl script
    git-svn-id: file:///usr/local/SVN/trunk@547 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- PhylogenySurvey/perl scripts/TreeSurvey.pl
+++ PhylogenySurvey/perl scripts/TreeSurvey.pl
@@ -128,7 +128,7 @@ sub load_survey {
   	print "<form action=\"$script_url\" method=\"POST\" onsubmit=\"return getTreeData();\" ";
   	print "name=\"form\">\n";
 	print "<applet code=\"phylogenySurvey.SurveyApplet.class\" \n";
-	print "archive=\"https://www.securebio.umb.edu/phylogenySurvey.1.jar\" \n";
+	print "archive=\"https://www.securebio.umb.edu/phylogenySurvey.2.jar\" \n";
 	print "width=1020 height=1020 name=\"TreeApplet\">\n";
   	print "          You have to enable Java on your machine !</applet>\n";
     print "<br><br>\n";
--- PhylogenySurvey/src/phylogenySurvey/SurveyApplet.java
+++ PhylogenySurvey/src/phylogenySurvey/SurveyApplet.java
@@ -9,17 +9,22 @@ import javax.swing.JApplet;
 public class SurveyApplet extends JApplet {
 	
 	private SurveyUI surveyUI;
+	private boolean scoringEnabled;
+	private String password;
 	
 	public void init() {
-		boolean scoringEnabled = false;
+		scoringEnabled = false;
 		String scoreModeString = getParameter("Scoring");
-		String password = getParameter("Password");
+		password = getParameter("Password");
 		if ((scoreModeString != null) && (password != null)) {
 			if (scoreModeString.equals("true")) {
 				scoringEnabled = true;
 			}
 		}
 		setSize(800, 800);
+	}
+	
+	public void start() {
 		surveyUI = new SurveyUI(this.getContentPane());
 		surveyUI.setupUI(scoringEnabled, password);
 	}
