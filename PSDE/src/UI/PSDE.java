8b5992efd099c2aab745467e024f189c60e64c74
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2009-06-09 15:29:00 +0000

    *** empty log message ***
    git-svn-id: file:///usr/local/SVN/trunk@593 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- PSDE/src/UI/PSDE.java
+++ PSDE/src/UI/PSDE.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -114,6 +115,11 @@ public class PSDE extends JFrame {
 
 		startEMUItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					Process p = Runtime.getRuntime().exec("java -jar microemulator.jar");
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
 
