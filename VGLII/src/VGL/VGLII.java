81614606b288337f7530904c33dfa02840a4ecb5
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2009-09-21 17:53:44 +0000

    working on french
    git-svn-id: file:///usr/local/SVN/trunk@613 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- VGLII/src/VGL/VGLII.java
+++ VGLII/src/VGL/VGLII.java
@@ -736,15 +736,19 @@ public class VGLII extends JFrame {
 				+ "Release Version " + version + "<br>" + "Copyright 2009<br>" + "VGL Team:<br>" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 				+ "<ul>" //$NON-NLS-1$
 				+ "<li><b>Lead Programmer:</b></li>" //$NON-NLS-1$
-				+ "<ul><li>Brian White</li></ul>" //$NON-NLS-1$
+				+ "<ul><li>Brian White (University of Massachusetts, Boston)</li>"
+				+ "<li><i>brian.white@umb.edu</i></li></ul>" //$NON-NLS-1$
 				+ "<li><b>Original VGL UI Code:</b></li>" //$NON-NLS-1$
-				+ "<ul><li>Nikunj Koolar</li><li>Wei Ma</li><li>Naing Naing Maw</li>" //$NON-NLS-1$
-				+ "<li>Chung Ying Yu</li></ul>" //$NON-NLS-1$
+				+ "<ul><li>Nikunj Koolar (UMB)</li><li>Wei Ma (UMB)</li><li>Naing Naing Maw (UMB)</li>" //$NON-NLS-1$
+				+ "<li>Chung Ying Yu (UMB)</li></ul>" //$NON-NLS-1$
 				+ "<li><b>Phenotype Images:</b></li>" //$NON-NLS-1$
-				+ "<ul><li>Antoine Aidamouni</li><li>Sara M. Burke</li><li>Sara Hachemi</li>" //$NON-NLS-1$
-				+ "<li>Amit Kuma</li></ul>" //$NON-NLS-1$
+				+ "<ul><li>Antoine Aidamouni (UMB)</li><li>Sara M. Burke (UMB)</li><li>Sara Hachemi (UMB)</li>" //$NON-NLS-1$
+				+ "<li>Amit Kuma (UMB)</li></ul>" //$NON-NLS-1$
+				+ "<li><b>Translations:</b></li>"
+				+ "<ul><li>traduction française: Sophie Javerzat (Université Bordeaux 1, FRANCE)</li>"
+				+ "</ul>"
 				+ "<li><b>Academic Supervisor:</b></li>" //$NON-NLS-1$
-				+ "<ul><li>Ethan Bolker</li></ul>" //$NON-NLS-1$
+				+ "<ul><li>Ethan Bolker (UMB)</li></ul>" //$NON-NLS-1$
 				+"</ul>" //$NON-NLS-1$
 				+ "All Rights Reserved\n" + "GNU General Public License\n" //$NON-NLS-1$ //$NON-NLS-2$
 				+ "http://www.gnu.org/copyleft/gpl.html</body></html>"); //$NON-NLS-1$
