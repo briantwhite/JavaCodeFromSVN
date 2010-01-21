309f4b822c3eafa65f7da6358c64dfc41d4e95f4
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2010-01-21 16:37:02 +0000

    spanish is working
    git-svn-id: file:///usr/local/SVN/trunk@662 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- VGLII/src/VGL/VGLII.java
+++ VGLII/src/VGL/VGLII.java
@@ -89,7 +89,7 @@ public class VGLII extends JFrame {
 	/**
 	 * the version number
 	 */
-	public final static String version = "2.1.0"; //$NON-NLS-1$
+	public final static String version = "2.1.1"; //$NON-NLS-1$
 
 	/**
 	 * the list of supported languages
