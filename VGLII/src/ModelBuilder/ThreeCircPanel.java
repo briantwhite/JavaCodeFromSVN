5e939137e50ab839c69619545ba6025fddb5a9cf
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2010-12-15 20:45:23 +0000

    three circ now ok
    git-svn-id: file:///usr/local/SVN/trunk@839 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- VGLII/src/ModelBuilder/ThreeCircPanel.java
+++ VGLII/src/ModelBuilder/ThreeCircPanel.java
@@ -5,18 +5,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 import VGL.Messages;
 
-public class ThreeCircPanel extends ModelDetailsPanel implements ItemListener {
+public class ThreeCircPanel extends ModelDetailsPanel {
 	
-	JTextField t3ReDisplay;
+	JLabel l;
 
 	public ThreeCircPanel(String[] phenos,
 			JComboBox t1Choices,
@@ -31,35 +28,25 @@ public class ThreeCircPanel extends ModelDetailsPanel implements ItemListener {
 				
 		setLayout(new GridLayout(7,2));
 		
-		add(new JLabel(""));
 		add(t3Choices);
-		t3Choices.addItemListener(this);
+		add(new JLabel(""));
 		
-		add(new JLabel(""));
 		add(new JLabel(Messages.getInstance().getString("VGLII.IsDominantTo")));
-
 		add(new JLabel(""));
+
 		add(t2Choices);
-
 		add(new JLabel(""));
+
 		add(new JLabel(Messages.getInstance().getString("VGLII.IsDominantTo")));
-
 		add(new JLabel(""));
+
 		add(t1Choices);
-		
 		add(new JLabel(""));
-		add(new JLabel(Messages.getInstance().getString("VGLII.IsDominantTo")));
 		
-		// set up echo of t3
-		int maxLen = 0;
-		for (int i = 0; i < phenos.length; i++) {
-			if (phenos[i].length() > maxLen) maxLen = phenos[i].length();
-		}
-		t3ReDisplay = new JTextField(maxLen);
-		t3ReDisplay.setText(phenos[0]);
-		t3ReDisplay.setEditable(false);
+		l = new JLabel(Messages.getInstance().getString("VGLII.IsDominantTo"));
+		add(l);
 		add(new JLabel(""));
-		add(t3ReDisplay);
+		
 	}
 
 	public String[] getChoices() {
@@ -70,24 +57,19 @@ public class ThreeCircPanel extends ModelDetailsPanel implements ItemListener {
 		return r;
 	}
 
-	public void itemStateChanged(ItemEvent arg0) {
-		t3ReDisplay.setText(t3Choices.getSelectedItem().toString());
-	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D)g;
 		g2d.setColor(Color.BLACK);
 		g2d.setStroke(new BasicStroke(5F));
-		g2d.drawArc(50, 15, 80, t3ReDisplay.getLocation().y, 90, 180);
+		g2d.drawArc(70, 15, 80, l.getLocation().y, -90, 180);
 		
-		g2d.drawLine(90,15,100,15);
-		g2d.drawLine(95,10,100,15);
-		g2d.drawLine(95,20,100,15);
+		g2d.drawLine(95,15,105,15);
+		g2d.drawLine(105,10,95,15);
+		g2d.drawLine(105,20,95,15);
 		
-		g2d.drawLine(90,t3ReDisplay.getLocation().y + 15,100,t3ReDisplay.getLocation().y + 15);
-		g2d.drawLine(95,t3ReDisplay.getLocation().y + 20,100,t3ReDisplay.getLocation().y + 15);
-		g2d.drawLine(95,t3ReDisplay.getLocation().y + 10,100,t3ReDisplay.getLocation().y + 15);
+		g2d.drawLine(95,l.getLocation().y + 15,105,l.getLocation().y + 15);
 	}
 
 
