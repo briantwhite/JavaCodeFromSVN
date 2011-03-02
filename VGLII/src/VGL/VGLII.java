575c835d0d2b96587bf71a3ab16c5664ea4c398f
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2011-03-02 22:35:20 +0000

    made Getworkashtml (cleaning up VGLII.java)
    git-svn-id: file:///usr/local/SVN/trunk@932 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- VGLII/src/VGL/VGLII.java
+++ VGLII/src/VGL/VGLII.java
@@ -179,7 +179,7 @@ public class VGLII extends JFrame {
 	/**
 	 * The filter type to display only Work files
 	 */
-	private static final String wrkFilterString = new String("wr2"); //$NON-NLS-1$
+	public static final String wrkFilterString = new String("wr2"); //$NON-NLS-1$
 
 	/**
 	 * The filter type to display Print files
@@ -1110,7 +1110,7 @@ public class VGLII extends JFrame {
 				new OutputStreamWriter(
 						new BufferedOutputStream(
 								new FileOutputStream(printFile)),"ISO8859_1");
-			op.write(getWorkAsHTML());
+			op.write(GetWorkAsHTML.getWorkAsHTML(cageCollection, modelBuilder));
 			op.flush();
 			op.close();
 		} catch (IOException e) {
@@ -1133,84 +1133,11 @@ public class VGLII extends JFrame {
 		docRenderer.setScaleWidthToFit(true);
 		JTextPane printTextPane = new JTextPane();
 		printTextPane.setContentType("text/html"); //$NON-NLS-1$
-		printTextPane.setText(getWorkAsHTML());
+		printTextPane.setText(GetWorkAsHTML.getWorkAsHTML(cageCollection, modelBuilder));
 		HTMLDocument htDoc = (HTMLDocument) printTextPane.getDocument();
 		docRenderer.print(htDoc);
 	}
 
-	/**
-	 * generate an html representation of the current work
-	 * used by print() and saveToServer()
-	 *
-	 */
-	private String getWorkAsHTML() {
-		StringBuffer htmlString = new StringBuffer();
-		htmlString.append("<html><body>"); //$NON-NLS-1$
-		for (int i = 0; i < cageCollection.size(); i++) {
-			Cage c = (cageCollection.get(i)).getCage();
-			ArrayList<Organism> parents = c.getParents();
-			Organism parent1 = (Organism) parents.get(0);
-			Organism parent2 = (Organism) parents.get(1);
-			TreeMap<String, OrganismList> children = c.getChildren();
-			int id = c.getId();
-			htmlString.append("<table border=1><tr><td align=center colspan=3" //$NON-NLS-1$
-					+ " bgcolor=#C0C0C0>Cage " + (id + 1) + "</td></tr>"); //$NON-NLS-1$ //$NON-NLS-2$
-			htmlString.append("<tr><td nowrap colspan=3>");
-			htmlString.append(Messages.getInstance().getInstance().getString("VGLII.Parents")); //$NON-NLS-1$
-			if (parent1 != null && parent2 != null) {
-				htmlString.append("<ul><li>" + parent1.getSexString() + " " //$NON-NLS-1$ //$NON-NLS-2$
-						+ Messages.getInstance().getInstance().translateLongPhenotypeName(parent1.getPhenotypeString()) + " " 
-						+ Messages.getInstance().getInstance().getString("VGLII.FromCage") //$NON-NLS-1$
-						+ (parent1.getCageId() + 1));
-				htmlString.append("<li>" + parent2.getSexString() + " " //$NON-NLS-1$ //$NON-NLS-2$
-						+ Messages.getInstance().getInstance().translateLongPhenotypeName(parent2.getPhenotypeString()) + " " 
-						+ Messages.getInstance().getInstance().getString("VGLII.FromCage") //$NON-NLS-1$
-						+ (parent2.getCageId() + 1) + "</ul>"); //$NON-NLS-1$
-			}
-			htmlString.append("</td></tr>"); //$NON-NLS-1$
-			htmlString.append("<tr><td nowrap align=center colspan=3>");
-			htmlString
-			.append(Messages.getInstance().getInstance().getString("VGLII.Offspring")); //$NON-NLS-1$
-			htmlString.append("</td></tr>");
-			htmlString.append("<tr><td nowrap align=center>");
-			htmlString.append(Messages.getInstance().getInstance().getString("VGLII.Phenotype") //$NON-NLS-1$
-					+ "</td>"
-					+ "<td nowrap align=center>"
-					+ Messages.getInstance().getInstance().getString("VGLII.Sex") //$NON-NLS-1$
-					+ "</td>"
-					+ "<td nowrap align=center>"
-					+ Messages.getInstance().getInstance().getString("VGLII.Count")
-					+ "</td></tr>"); //$NON-NLS-1$
-			Iterator<String> it = children.keySet().iterator();
-			while (it.hasNext()) {
-				String phenotype = it.next();
-				OrganismList l = children.get(phenotype);
-
-				htmlString.append("<tr><td nowrap align=center>" 
-						+ Messages.getInstance().getInstance().translateLongPhenotypeName(phenotype) //$NON-NLS-1$
-						+ "</td>" + "<td nowrap align=center>" + Messages.getInstance().getInstance().getString("VGLII.Male") //$NON-NLS-1$ //$NON-NLS-2$
-						+ "</td><td nowrap align=center>" + l.getNumberOfMales() //$NON-NLS-1$
-						+ "</td></tr>"); //$NON-NLS-1$
-				htmlString.append("<tr><td nowrap align=center>" 
-						+ Messages.getInstance().getInstance().translateLongPhenotypeName(phenotype) //$NON-NLS-1$
-						+ "</td>" 
-						+"<td nowrap align=center>"
-						+ Messages.getInstance().getInstance().getString("VGLII.Female") //$NON-NLS-1$ //$NON-NLS-2$
-						+ "</td><td nowrap align=center>" + l.getNumberOfFemales() //$NON-NLS-1$
-						+ "</td></tr>"); //$NON-NLS-1$
-			}
-			htmlString.append("</table><p></p>"); //$NON-NLS-1$
-		}
-
-		htmlString.append("<br><br><hr>");
-		htmlString.append("<h3>");
-		htmlString.append(Messages.getInstance().getString("VGLII.ModelBuilder"));
-		htmlString.append("</h3>");
-		htmlString.append(modelBuilder.getAsHtml());
-
-		htmlString.append("</body></html>"); //$NON-NLS-1$
-		return htmlString.toString();
-	}
 
 	/**
 	 * Closes the problem that the user has been working on so far and releases
