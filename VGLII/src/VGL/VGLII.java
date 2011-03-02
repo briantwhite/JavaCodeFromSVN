1513f96a160fa2f55eea7ac23e675313ef924fad
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2011-03-02 22:18:18 +0000

    made showhelpinfo (cleaning VGLII.java)
    git-svn-id: file:///usr/local/SVN/trunk@931 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- VGLII/src/VGL/VGLII.java
+++ VGLII/src/VGL/VGLII.java
@@ -1364,59 +1364,7 @@ public class VGLII extends JFrame {
 	 * This method invokes .html help into a JEditor pane
 	 */
 	private void onlineHelp() {
-		final JEditorPane helpPane = new JEditorPane();
-		helpPane.setEditable(false);
-		helpPane.setContentType("text/html"); //$NON-NLS-1$
-
-		try {
-			helpPane.setPage(VGLII.class.getResource(
-					Messages.getInstance().getInstance().getString("VGLII.HelpFileDir") 
-					+ "/"
-					+ "index.html")); //$NON-NLS-1$
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(this,
-					Messages.getInstance().getInstance().getString("VGLII.HelpWarning1"), //$NON-NLS-1$
-					Messages.getInstance().getInstance().getString("VGLII.HelpWarning2"), JOptionPane.ERROR_MESSAGE); //$NON-NLS-1$
-			return;
-		}
-
-		JScrollPane helpScrollPane = new JScrollPane(helpPane);
-		JDialog helpDialog = new JDialog(this, Messages.getInstance().getInstance().getString("VGLII.HelpPage")); //$NON-NLS-1$
-		JButton backButton = new JButton(Messages.getInstance().getInstance().getString("VGLII.ReturnToTop")); //$NON-NLS-1$
-		helpDialog.getContentPane().setLayout(new BorderLayout());
-		helpDialog.getContentPane().add(backButton, BorderLayout.NORTH);
-		helpDialog.getContentPane().add(helpScrollPane, BorderLayout.CENTER);
-		Dimension screenSize = getToolkit().getScreenSize();
-		helpDialog.setBounds((screenSize.width / 8), (screenSize.height / 8),
-				(screenSize.width * 8 / 10), (screenSize.height * 8 / 10));
-		helpDialog.setVisible(true);
-
-		helpPane.addHyperlinkListener(new HyperlinkListener() {
-			public void hyperlinkUpdate(HyperlinkEvent e) {
-				if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-					try {
-						helpPane.setPage(e.getURL());
-					} catch (IOException ioe) {
-						System.err.println(ioe.toString());
-					}
-				}
-			}
-
-		});
-
-		backButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				try {
-					helpPane.setPage(VGLII.class.getResource(
-							Messages.getInstance().getInstance().getString("VGLII.HelpFileDir") 
-							+ System.getProperty("file.separator")
-							+ "index.html")); //$NON-NLS-1$
-				} catch (Exception e) {
-					System.err
-					.println(Messages.getInstance().getInstance().getString("VGLII.HelpWarning3") + e.toString()); //$NON-NLS-1$
-				}
-			}
-		});
+		ShowHelpInfo.showHelpInfo(this);
 	}
 
 	/**
