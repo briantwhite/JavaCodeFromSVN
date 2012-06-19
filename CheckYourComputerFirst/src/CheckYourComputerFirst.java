235efb252407360649e5522bfb19a463f3749855
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2012-06-19 19:49:52 +0000

    working on detecting if in zip file
    git-svn-id: file:///usr/local/SVN/trunk@1246 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- CheckYourComputerFirst/src/CheckYourComputerFirst.java
+++ CheckYourComputerFirst/src/CheckYourComputerFirst.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,20 +17,42 @@ public class CheckYourComputerFirst {
 	static int firstDigit;
 	static int secondDigit;
 	static String infoString;
-	
+
 	public static void main(String[] args) {
 		label = new JLabel();
-		frame = new JFrame("Check Your Computer First 1.0");
+		frame = new JFrame("Check Your Computer First 2.0");
 		frame.getContentPane().add(label);
-		
+
 		javaVersion = System.getProperty("java.version");
 		versionPieces = javaVersion.split("[^0-9]");
 		firstDigit = Integer.parseInt(versionPieces[0]);
 		secondDigit = Integer.parseInt(versionPieces[1]);
-		
+
 		label.setOpaque(true);
 		
-		if ((firstDigit >= 1) && (secondDigit >= 4)) {
+		String pwdCommand = "pwd";
+		if (System.getProperty("os.name").toLowerCase().indexOf("win") != -1) {
+			pwdCommand = "echo %cd%";
+		}
+
+		Runtime rt = Runtime.getRuntime();
+		Process p;
+		StringBuffer result = new StringBuffer();
+		try {
+			p = rt.exec(pwdCommand);
+			p.waitFor();
+			BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = reader.readLine();
+			while(line != null) {
+				result.append(line);
+				line = reader.readLine();
+			}
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		if ((firstDigit >= 1) && (secondDigit >= 6)) {
 			label.setBackground(Color.GREEN);
 			infoString = "You have a correct version of Java "
 				+ "installed on your computer.<br>"
@@ -41,12 +66,14 @@ public class CheckYourComputerFirst {
 				+ "or go to http://java.sun.com to download the"
 				+ " latest version of Java for free.<br>";
 		}
-		
+
 		label.setText("<html><body>"
 				+ infoString + "<br>"
 				+ "Java version " + javaVersion
+				+ "<br>"
+				+ result.toString()
 				+ "</font></body></html>");
-		
+
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -54,7 +81,7 @@ public class CheckYourComputerFirst {
 		});
 		frame.pack();
 		frame.setLocation(200,200);
-		frame.show();
+		frame.setVisible(true);
 
 	}
 
