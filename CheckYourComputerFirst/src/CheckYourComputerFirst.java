7e702c6788fad12cfb3288ffd5bb9190e0e64a0f
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2012-06-20 15:16:29 +0000

    working on zip detection
    git-svn-id: file:///usr/local/SVN/trunk@1248 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- CheckYourComputerFirst/src/CheckYourComputerFirst.java
+++ CheckYourComputerFirst/src/CheckYourComputerFirst.java
@@ -30,19 +30,36 @@ public class CheckYourComputerFirst {
 
 		label.setOpaque(true);
 		
+		boolean isInZip = false;
+		if (System.getProperty("user.dir").toLowerCase().contains("\\windows\\system")) isInZip = true;
+		
 		if ((firstDigit >= 1) && (secondDigit >= 6)) {
 			label.setBackground(Color.GREEN);
 			infoString = "You have a correct version of Java "
 				+ "installed on your computer.<br>"
-				+ "The software on this CD should run correctly.<br>";
+				+ "The software in this folder should run correctly.<br>";
+			if (isInZip) {
+				label.setBackground(Color.YELLOW);
+				infoString += "However, it appears that you have not <br>"
+					+ "un-zipped the folder containing this software.<br>"
+					+ "The software will not run properly unless you <br>" 
+					+ "un-zip it. See the instructions where you downloaded<br>"
+					+ "these files for how to un-zip properly.";
+			}
 		} else {
 			label.setBackground(Color.RED);
 			infoString = "It appears that you do not have a correct "
 				+ "version of Java installed on your computer.<br>"
-				+ "The software on this CD will not run correctly.<br>"
-				+ "You should install the version of Java found on this CD<br>"
-				+ "or go to http://java.sun.com to download the"
+				+ "The software in this folder will not run correctly.<br>"
+				+ "You should install by going to http://www.java.com to download the"
 				+ " latest version of Java for free.<br>";
+			if (isInZip) {
+				infoString += "Also, it appears that you have not <br>"
+					+ "un-zipped the folder containing this software.<br>"
+					+ "The software will not run properly unless you <br>" 
+					+ "un-zip it. See the instructions where you downloaded<br>"
+					+ "these files for how to un-zip properly.";
+			}			
 		}
 
 		label.setText("<html><body>"
