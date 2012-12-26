dc89f2b495f605938a83f7e98d8ff0f52af3df38
brian <brian@f794847c-437f-4cdd-b6dc-fa27500bcbe7>
2012-12-26 15:03:07 +0000

    applet is working
    git-svn-id: file:///usr/local/SVN/trunk@1442 f794847c-437f-4cdd-b6dc-fa27500bcbe7

--- NewProtex/src/protex/ProtexApplet.java
+++ NewProtex/src/protex/ProtexApplet.java
@@ -1,12 +1,13 @@
 package protex;
 
 import java.awt.Dimension;
-import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JApplet;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ProtexApplet extends JApplet {
@@ -15,11 +16,15 @@ public class ProtexApplet extends JApplet {
 	private Protex protex;
 
 	public void init() {
+		
 		protex = new Protex(true);
 		foldingWindow = new FoldingWindow("Folding Window", protex);
 		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new GridLayout(1,2));
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
 
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+		
 		JPanel aapPanel = new JPanel();
 		aapPanel.setBorder(BorderFactory.createTitledBorder("Amino acids"));
 		aapPanel.setLayout(new BoxLayout(aapPanel, BoxLayout.X_AXIS));
@@ -27,7 +32,18 @@ public class ProtexApplet extends JApplet {
 		aapPanel.setMaximumSize(new Dimension(250, 200));
 		aapPanel.add(aaPalette);
 		aapPanel.add(Box.createRigidArea(new Dimension(1,180)));
-		mainPanel.add(aapPanel);
+		leftPanel.add(aapPanel);
+		
+	    String targetShapeString = getParameter("TARGET_SHAPE");
+	    if (targetShapeString != null) {
+	    	ProteinImageSet pis = ProteinImageFactory.buildProtein(targetShapeString, false);
+	    	JPanel targetPanel = new JPanel();
+	    	targetPanel.setBorder(BorderFactory.createTitledBorder("Target Shape"));
+	    	targetPanel.add(new JLabel(new ImageIcon(pis.getFullScaleImage())));
+	    	leftPanel.add(targetPanel);
+	    }
+
+		mainPanel.add(leftPanel);
 		mainPanel.add(foldingWindow);
 
 		getContentPane().add(mainPanel);
