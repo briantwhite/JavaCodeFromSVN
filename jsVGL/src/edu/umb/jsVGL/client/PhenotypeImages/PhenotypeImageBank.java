8c747f6c4e4c2fcca62c3a189064ec3422f1a4aa
brianwhite <brian.white@umb.edu>
2013-03-05 09:35:56 -0500

    adding images

---
+++ jsVGL/src/edu/umb/jsVGL/client/PhenotypeImages/PhenotypeImageBank.java
@@ -0,0 +1,43 @@
+package edu.umb.jsVGL.client.PhenotypeImages;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.ImageIcon;
+
+import GeneticModels.Phenotype;
+
+public class PhenotypeImageBank {
+	
+	private static PhenotypeImageBank instance;
+	
+	private HashMap<String, ImageIcon> bank;
+	
+	private PhenotypeImageMaker imageMaker;
+	
+	private PhenotypeImageBank() {
+		bank = new HashMap<String, ImageIcon>();
+		imageMaker = new PhenotypeImageMaker();
+	}
+	
+	public static PhenotypeImageBank getInstance() {
+		if (instance == null) {
+			instance = new PhenotypeImageBank();
+		}
+		return instance;
+	}
+	
+	public void resetDefaults() {
+		imageMaker = new PhenotypeImageMaker();
+	}
+	
+	public ImageIcon getImageForPhenotype(
+			ArrayList<Phenotype> phenotypes, 
+			String phenoString) {
+		if (!bank.containsKey(phenoString)) {
+			bank.put(phenoString, imageMaker.makeImage(phenotypes));
+		}
+		return bank.get(phenoString);
+	}
+	
+}
