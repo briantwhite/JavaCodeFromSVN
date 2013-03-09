859a09a5cce5898a326be56a5f235c9e8317f2aa
Brian White <brian.white@umb.edu>
2013-03-09 12:06:48 -0500

    working on starting page

--- jsVGL/src/edu/umb/jsVGL/client/JsVGL.java
+++ jsVGL/src/edu/umb/jsVGL/client/JsVGL.java
@@ -1,52 +1,74 @@
 package edu.umb.jsVGL.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class JsVGL implements EntryPoint {
+	
+	private final static String WELCOME_HTML = "<html><body>"
+			+ "<h3>Welcome to VGL</h3>"
+			+ "You can either:"
+			+ "<ul><li>Start a new problem by clicking the &quot;New Problem&quot; button above.</li>"
+			+ "<li>Open work you have saved on this problem by clicking the &quot;Open Work&quot; button above.</li></ul>"
+			+ "</body></html>";
+	
+	private Button newProblemButton = null;
+	private Button openWorkButton = null;
+	private Button saveWorkButton = null;
+	private Button crossButton = null;
+	private Button superCrossButton = null;
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button newProblemButton = new Button("NewProblem");
-
-		// We can add style names to widgets
-		newProblemButton.addStyleName("newProblemButton");
-
+		newProblemButton = new Button("New Problem");
 		RootPanel.get("newProblemButtonContainer").add(newProblemButton);
+		
+		openWorkButton = new Button("Open Work");
+		RootPanel.get("openWorkButtonContainer").add(openWorkButton);
+		
+		saveWorkButton = new Button("Save Work");
+		RootPanel.get("saveWorkButtonConatiner").add(saveWorkButton);
+		saveWorkButton.setEnabled(false);
+		
+		crossButton = new Button("Cross Two");
+		RootPanel.get("crossButtonContainer").add(crossButton);
+		crossButton.setEnabled(false);
 
+		superCrossButton = new Button("Super Cross");
+		if (RootPanel.get("superCrossButtonContainer") != null) {
+			RootPanel.get("superCrossButtonContainer").add(superCrossButton);
+		}
+		superCrossButton.setEnabled(false);
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
+		final TabPanel mainPanel = new TabPanel();
+		
+		final FlowPanel instructionsPanel = new FlowPanel();
+		instructionsPanel.add(new HTML(WELCOME_HTML));
+		mainPanel.add(instructionsPanel, "Instructions");
+		
+		final FlowPanel modelBuilderPanel = new FlowPanel();
+		modelBuilderPanel.add(new Label("MBUI"));
+		mainPanel.add(modelBuilderPanel, "Model Builder");
+		
+		final FlowPanel summaryChartPanel = new FlowPanel();
+		summaryChartPanel.add(new Label("SCUI"));
+		mainPanel.add(summaryChartPanel, "Summary Chart");
+		
+		mainPanel.selectTab(0);
+		mainPanel.setSize("500px", "250px");
+		mainPanel.addStyleName("table-center");
+		RootPanel.get("mainPanelContainer").add(mainPanel);
+		
 
 	}
 }
