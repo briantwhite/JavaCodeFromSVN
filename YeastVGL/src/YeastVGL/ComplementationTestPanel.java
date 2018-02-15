1545f9ec38d0241ed640b5c5db3f59d985418bb5
brianwhite <brianwhite@147.210.54.80>
2018-02-15 15:51:04 +0100

    complementation panel ok with compl grp entry and working set checkboxes

--- YeastVGL/src/YeastVGL/ComplementationTestPanel.java
+++ YeastVGL/src/YeastVGL/ComplementationTestPanel.java
@@ -3,6 +3,9 @@ package YeastVGL;
 import java.awt.Dimension;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -22,7 +25,8 @@ public class ComplementationTestPanel extends JPanel implements TableColumnModel
 	private int numMutants;
 	private int numEnzymes;
 	private String[] columnHeadings;
-	private JCheckBox[] checkboxes;
+	private JCheckBox[] workingSetCheckboxes;
+
 	private Object[][] data;
 
 	public ComplementationTestPanel(YeastVGL yeastVGL) {
@@ -31,35 +35,52 @@ public class ComplementationTestPanel extends JPanel implements TableColumnModel
 		numMutants = mutantSet.getNumberOfMutants();
 		numEnzymes = yeastVGL.getPathway().getNumberOfEnzymes();
 
+		// set up the column names
 		String[] mutantNames = new String[numMutants];
-		columnHeadings = new String[numMutants + 1];
+		columnHeadings = new String[numMutants + 2];
 		columnHeadings[0] = new String("");
 		for (int i = 0; i < numMutants; i++) {
 			mutantNames[i] = new String("M" + i);
 			columnHeadings[i + 1] = new String("M" + i);
 		}
+		columnHeadings[numMutants + 1] = new String("Group");	
 
+		// compute the complementation table
 		ArrayList<Integer>startingMaterials = new ArrayList<Integer>();
 		startingMaterials.add(new Integer(0));
-		data = new Object[numMutants][numMutants + 1];
+		data = new Object[numMutants][numMutants + 2];
 		for (int row = 0; row < numMutants; row++) {
-			for (int col = 0; col < (numMutants + 1); col++) {
+			for (int col = 0; col < (numMutants + 2); col++) {
 				if (col == 0) {
 					data[row][col] = new String(mutantNames[row]);
+				} else if (col == (numMutants + 1)){
+					data[row][col] = new String("-enter name-");
 				} else {
 					data[row][col] = willDiploidGrow(row, col - 1, startingMaterials);
 				}
 			}
 		}
 
-		checkboxes = new JCheckBox[columnHeadings.length];
-		for (int i = 1; i < columnHeadings.length; i++) {
-			checkboxes[i] = new JCheckBox();
-		}
+		// set up the UI
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.add(Box.createRigidArea(new Dimension(700,10)));
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+		
+		JPanel leftPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+		leftPanel.add(Box.createRigidArea(new Dimension(600,1)));
+		
+		JPanel ctp = new JPanel();
+		ctp.setBorder(BorderFactory.createTitledBorder("Complementation Table"));
 		complementationTable = new JTable();
+		// make sure you can't drag the first or last columns
 		complementationTable.setColumnModel(new DefaultTableColumnModel() {
 			public void moveColumn(int columnIndex, int newIndex) {
-				if ((columnIndex == 0) || (newIndex == 0)) {
+				if ((columnIndex == 0) || (newIndex == 0) 
+						|| (columnIndex == (columnHeadings.length - 1))
+						|| (newIndex == (columnHeadings.length - 1))) {
 					return;
 				}
 				super.moveColumn(columnIndex, newIndex);
@@ -69,13 +90,42 @@ public class ComplementationTestPanel extends JPanel implements TableColumnModel
 		complementationTable.setFillsViewportHeight(true);
 		complementationTable.getColumnModel().addColumnModelListener(this);
 		for (int i = 0; i < columnHeadings.length; i++) {
-			complementationTable.getColumnModel().getColumn(i).setPreferredWidth(30);
+			if (i == (columnHeadings.length - 1)) {
+				complementationTable.getColumnModel().getColumn(i).setPreferredWidth(100);
+			} else {
+				complementationTable.getColumnModel().getColumn(i).setPreferredWidth(30);
+			}
 		}
-		complementationTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		//		complementationTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		complementationTable.setRowHeight(30);
 		JScrollPane tablePane = new JScrollPane(complementationTable);
 		tablePane.setPreferredSize(new Dimension(complementationTable.getPreferredSize().width + 30, 
 				complementationTable.getRowHeight() * (columnHeadings.length + 1)));
-		this.add(tablePane);
+		ctp.add(tablePane);
+		leftPanel.add(ctp);
+		mainPanel.add(leftPanel);
+		
+		JPanel rightPanel = new JPanel();
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+		rightPanel.add(Box.createRigidArea(new Dimension(100,1)));
+		
+		JPanel wsp = new JPanel();
+		wsp.setLayout(new BoxLayout(wsp, BoxLayout.Y_AXIS));
+		wsp.setBorder(BorderFactory.createTitledBorder("Working Set"));
+		wsp.add(Box.createRigidArea(new Dimension(100,1)));
+		// now the working set of mutants
+		workingSetCheckboxes = new JCheckBox[numMutants];
+		for (int i = 0; i < numMutants; i++) {
+			workingSetCheckboxes[i] = new JCheckBox("M" + i);
+		}
+		for (int i = 0; i < workingSetCheckboxes.length; i++) {
+			wsp.add(workingSetCheckboxes[i]);
+		}
+		rightPanel.add(wsp);
+		mainPanel.add(rightPanel);
+		
+		this.add(mainPanel);
+		
 		this.revalidate();
 	}
 
@@ -98,6 +148,7 @@ public class ComplementationTestPanel extends JPanel implements TableColumnModel
 		}
 	}
 
+
 	class ComTabModel extends DefaultTableModel {
 		public int getRowCount() { 
 			return data.length;
@@ -109,13 +160,21 @@ public class ComplementationTestPanel extends JPanel implements TableColumnModel
 			return columnHeadings[col];
 		}
 		public boolean isCellEditable(int r, int c) {
-			return false;
+			if (c == (columnHeadings.length - 1)) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 		public Object getValueAt(int rowIndex, int columnIndex) {
 			return data[rowIndex][columnIndex];
 		}
+		public void setValueAt(Object value, int row, int col) {
+			data[row][col] = value;
+			fireTableCellUpdated(row, col);
+		}
 	}
-	
+
 	// detect dragged columns and update rows accordingly
 	public void columnAdded(TableColumnModelEvent e) {}
 	public void columnRemoved(TableColumnModelEvent e) {}
