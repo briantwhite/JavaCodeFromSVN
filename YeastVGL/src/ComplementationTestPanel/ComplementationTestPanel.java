bc469c773c548a9eb1ff87aa0cae80235af440a7
brianwhite <brianwhite@147.210.54.80>
2018-03-07 11:43:51 +0100

    done refactoring

--- YeastVGL/src/ComplementationTestPanel/ComplementationTestPanel.java
+++ YeastVGL/src/ComplementationTestPanel/ComplementationTestPanel.java
@@ -101,23 +101,7 @@ public class ComplementationTestPanel extends JPanel implements ActionListener, 
 		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
 		leftPanel.add(Box.createRigidArea(new Dimension(150,1)));
 		
-		JPanel instructionPanel = new JPanel();
-		instructionPanel.setLayout(new BoxLayout(instructionPanel, BoxLayout.Y_AXIS));
-		instructionPanel.setBorder(BorderFactory.createTitledBorder("Instructions"));
-		instructionPanel.add(Box.createRigidArea(new Dimension(150,1)));
-		instructionPanel.add(new JLabel("<html>"
-				+ "<ol>"
-				+ "<li>Determine your complementation groups using the data at the left</li>"
-				+ "<ul>"
-				+ "<li>You can drag the data columns to the left or right to make it easier to see the groups.</li>"
-				+ "<li>You can save your work along the way.</li>"
-				+ "</ul>"
-				+ "<li>You should then assign a complementation group letter to each group by clicking in the corresponding cell"
-				+ " and choosing from the list that pops up.</li>"
-				+ "<li>Choose one member of each group for your working set by checking the box at the right.</li>"
-				+ "<li>Save your work before continuing to the Pathway pane.</li>"
-				+ "</ol>"
-				+ "</html>"));
+		JPanel instructionPanel = new CTPInstructionPanel();
 		leftPanel.add(instructionPanel);
 		mainPanel.add(leftPanel);
 		
