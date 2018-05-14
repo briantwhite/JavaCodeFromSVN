14edfdab5bd21af3dea2918827bafb9d23fe4e98
brianwhite <brianwhite@147.210.54.80>
2018-05-14 10:53:34 +0200

    YeastVGL - working on layout etc of Pathway panel

--- YeastVGL/src/PathwayPanel/MutantStrainCheckbox.java
+++ YeastVGL/src/PathwayPanel/MutantStrainCheckbox.java
@@ -9,8 +9,9 @@ public class MutantStrainCheckbox extends JCheckBox {
 	private SingleMutantStrain mutantStrain;
 	
 	public MutantStrainCheckbox(SingleMutantStrain mutantStrain) {
-		super("Mutation found in strain M" + mutantStrain.getIndex() + 
-				"; Complementation Group: " + mutantStrain.getComplementationGroup());
+		super("<html><font color='red'>Mutation found in strain M" + mutantStrain.getIndex() + 
+				": CG " + mutantStrain.getComplementationGroup()
+				+ "</font></html>");
 		this.mutantStrain = mutantStrain;
 	}
 	
