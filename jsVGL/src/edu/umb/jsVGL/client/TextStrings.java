5b723c79878d5bc334b247699ff9a46b44edea0f
Brian White <brian.white@umb.edu>
2019-11-08 13:18:48 -0500

    changed info strings in jsVGL

--- jsVGL/src/edu/umb/jsVGL/client/TextStrings.java
+++ jsVGL/src/edu/umb/jsVGL/client/TextStrings.java
@@ -7,10 +7,15 @@ public class TextStrings {
 	public static final String WELCOME_TEXT = "<html><body>"
 			+ "<h3>Welcome to js VGL version " + VERSION + "</h3>"
 			+ "<h3>Working a problem</h3>"
-			+ "You solve jsVGL problems by crossing organisms - select a male and a female symbol and then click the <b>Cross Two</b> button."
+			+ "You solve jsVGL problems by crossing organisms - select a male and a female symbol and then click the <b>Cross Two</b> button. "
 			+ "jsVGL will then generate the resulting offspring. By choosing your crosses and observing the results carefully, you can determine "
 			+ "how the trait(s) in your problem are inherited. You then enter your findings into the <b>Genetic Model</b> tab. Once you are sure"
 			+ " of your answer, you can then submit it for grading."
+			+ "<h4>Practice Problems</h4>"
+			+ "If you choose a new Practice Problem, you will be able to view the correct underlying model in the Genetic Model tab."
+			+ "<h4>Graded Problems</h4>"
+			+ "If you choose a new graded problem, you will not be able to view the underlying genetic model; you must, instead, "
+			+ "determine it for yourself."
 			+ "</body></html>";
 
 	public static final String ABOUT_jsVGL = 
