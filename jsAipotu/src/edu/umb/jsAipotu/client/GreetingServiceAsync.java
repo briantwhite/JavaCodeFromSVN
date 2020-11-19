86936921c949f46f795ac0cbc93e9cbe28c2037a
Brian White <brian.white@umb.edu>
2020-11-19 13:32:30 -0500

    initial commit

---
+++ jsAipotu/src/edu/umb/jsAipotu/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package edu.umb.jsAipotu.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
